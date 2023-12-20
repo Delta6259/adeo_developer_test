d432588d0706eb6387f20f816e2aeeadf10627ed
Guillaume TRUPIN <gtrupin@gmail.com>
2023-12-20 12:54:10 +0100

    fix: add missing type call.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import countries from "./data";
 import {count, filter} from "./countries-operations";
+import {Country} from "./types";
 
 const run = () => {
     const filterArgument: string | undefined = process.env.npm_config_filter;
     const countArgument: string | undefined = process.env.npm_config_count;
 
     if (filterArgument) {
-        const filteredCountries = filter(countries, filterArgument);
+        const filteredCountries: Country[] = filter(countries, filterArgument);
         if (filteredCountries.length) {
             console.log(JSON.stringify(filteredCountries));
         }
